183f0a6ccfbbeff170c73c77981997fa747c827d
Bruno Pimentel <bpimentel@mastertech.tech>
2018-10-18 19:58:32 -0300

    Adiciona sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 gulp.task('recarregar', () => {
     browserSync.init({server: './'});
-    gulp.watch(['index.html', 'css/main.css'], browserSync.reload);
 });
 
 gulp.task('sass', function () {
     return gulp.src('./scss/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
-  });
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+});
 
 gulp.task('observar', () => {
     //executa a tarefa concatenar.js quando ocorrem modificações no arquivo scripts.js
     gulp.watch(['scss/*.scss'], ['sass']);
+    gulp.watch(['index.html', 'css/main.css'], browserSync.reload);
 });
 
 gulp.task('default', ['sass', 'recarregar', 'observar']);
