3ad13fc61c5a3cf1d8e717f8b52fd40578331858
Bruno Pimentel <bpimentel@mastertech.tech>
2018-10-18 21:15:42 -0300

    Adiciona arquivo de js

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ gulp.task('concatenar', () => {
     return gulp.src([
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/popper.js/dist/umd/popper.min.js', 
-        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'js/scripts.js',
     ])
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest('dist'));
